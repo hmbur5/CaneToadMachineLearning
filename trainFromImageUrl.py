b081138671489894f405aced26456c1c28eb5c0b
Hannah Burke <hmbur5@student.monash.edu>
2020-08-28 10:05:27 +1000

    code to do all species similar to cane toads

--- trainFromImageUrl.py
+++ trainFromImageUrl.py
@@ -24,7 +24,7 @@ for project in trainer.get_projects():
     if project.name == 'Cane Toad Classifier Python':
         break
 # iteration name must be changed each iteration to publish
-publish_iteration_name = "classifyModel_large2"
+publish_iteration_name = "classifyModel_moreSpecies"
 
 
 
@@ -34,19 +34,28 @@ print("Adding images...")
 
 
 # getting image urls using ALA file for each species (iterative as we can only upload 64 images at a time)
-for species in ['caneToad', 'stripedMarshFrog', 'ornateBurrowingFrog']:
+for species in ['caneToad', 'stripedMarshFrog', 'ornateBurrowingFrog', 'australianGreenTreeFrog', 'bumpyRockFrog',
+                'crawlingToadlet', 'daintyGreenTreeFrog', 'desertFroglet', 'desertTreeFrog', 'giantFrog', 'hootingFrog',
+                 'marbledFrog', 'moaningFrog', 'motorbikeFrog', 'newHollandFrog', 'rockholeFrog',
+                'rothsTreeFrog', 'westernBanjoFrog', 'whiteLippedTreeFrog']:
 
     # finding tag id
     for tag in trainer.get_tags(project.id):
         if tag.name == species:
             break
+    if tag.name!=species:
+        tag = trainer.create_tag(project.id, species)
 
     file_dir = 'ala image urls/' + species + 'RawFile.csv'
     image_url_list = GetALAimages.listOfAlaImageUrls(file_dir)
-    for batch_number in range(math.floor(len(image_url_list)/64)):
+
+    # going through a small portion of url list as can only upload 64 at a time
+    for batch_number in range(math.ceil(len(image_url_list)/64)):
         image_list = []
-        # going through a small portion of list as can only do 64 at a time
-        for url in image_url_list[batch_number*64:(batch_number+1)*64]:
+        endIndex = (batch_number+1)*64
+        if endIndex > len(image_url_list):
+            endIndex = len(image_url_list)
+        for url in image_url_list[batch_number*64: endIndex]:
             image_list.append(ImageUrlCreateEntry(url=url, tag_ids=[tag.id]))
 
 
@@ -64,7 +73,6 @@ for species in ['caneToad', 'stripedMarshFrog', 'ornateBurrowingFrog']:
                     print("Image batch upload failed.")
                     print("Image status: ", image.status)
                     print("Image url: ", image.source_url)
-                    #exit(-1)
             if len(image_list)>0:
                 upload_result = trainer.create_images_from_urls(project.id, ImageUrlCreateBatch(images=image_list))
             else:
