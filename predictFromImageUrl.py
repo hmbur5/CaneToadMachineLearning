6c021c1b25389250a89ed6ef9534078b6a554c74
Hannah Burke <hmbur5@student.monash.edu>
2020-09-01 16:30:54 +1000

    updating multitag project for testing with ALA images

--- predictFromImageUrl.py
+++ predictFromImageUrl.py
@@ -3,36 +3,96 @@ from azure.cognitiveservices.vision.customvision.training.models import ImageFil
 from msrest.authentication import ApiKeyCredentials
 from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
 from msrest.authentication import ApiKeyCredentials
+import csv
+from PIL import Image
+import urllib
+from io import BytesIO
+import math
 
 
-def predict(image_url):
-
-    ENDPOINT = "https://canetoadmachinelearning.cognitiveservices.azure.com/"
-
-    training_key = "cde7deba2d5d4df5b768b50b700c46b7"
-    prediction_key = "fb49a542a16a47e6b68b2983db158c32"
-    prediction_resource_id = "/subscriptions/baa59b08-5ec4-44ea-a907-b12782d8e2a0/resourceGroups/Canetoads/providers/Microsoft.CognitiveServices/accounts/CaneToadMachineLea-Prediction"
+import os, ssl
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None)):
+    ssl._create_default_https_context = ssl._create_unverified_context
 
 
-    credentials = ApiKeyCredentials(in_headers={"Training-key": training_key})
-    trainer = CustomVisionTrainingClient(ENDPOINT, credentials)
+ENDPOINT = "https://canetoadmachinelearning.cognitiveservices.azure.com/"
 
-    project = trainer.get_project('c3656572-45da-465e-9614-3eb4a5041625')
-    publish_iteration_name = "classifyModel_urls"
-
-    # Now there is a trained endpoint that can be used to make a prediction
-    prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
-    predictor = CustomVisionPredictionClient(ENDPOINT, prediction_credentials)
+training_key = "cde7deba2d5d4df5b768b50b700c46b7"
+prediction_key = "fb49a542a16a47e6b68b2983db158c32"
+prediction_resource_id = "/subscriptions/baa59b08-5ec4-44ea-a907-b12782d8e2a0/resourceGroups/Canetoads/providers/Microsoft.CognitiveServices/accounts/CaneToadMachineLea-Prediction"
 
 
+credentials = ApiKeyCredentials(in_headers={"Training-key": training_key})
+trainer = CustomVisionTrainingClient(ENDPOINT, credentials)
 
-    results = predictor.classify_image_url(
-        project.id, publish_iteration_name, image_url)
+# finding project id
+for project in trainer.get_projects():
+    if project.name == 'Cane Toad Classifier Python':
+        break
+publish_iteration_name = "AllImages"
 
-    # Display the results.
-    for prediction in results.predictions:
-        print("\t" + prediction.tag_name +
-              ": {0:.2f}%".format(prediction.probability * 100))
+# Now there is a trained endpoint that can be used to make a prediction
+prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
+predictor = CustomVisionPredictionClient(ENDPOINT, prediction_credentials)
 
 
-predict('https://images.ala.org.au/store/b/6/6/6/61f0ec42-362c-45d1-a1a3-69c415ac666b/original')
+
+
+
+# initialise list to store image urls from file
+testing_image_urls = []
+with open('predictions/multitagsAll.csv', 'r') as myfile:
+    for url in myfile:
+        url, species = url.split(',')
+        testing_image_urls.append([url, species])
+
+# get prediction percentages
+image_predictions = []
+for url,species in testing_image_urls:
+    try:
+        results = predictor.classify_image_url(project.id, publish_iteration_name, url)
+
+    # not sure what exception should be as custom vision gives a strange error.
+    except:
+        try:
+            # if images are too large for prediction (must be <4mb), use python to scale down
+            img = urllib.request.urlopen(url)
+            img = Image.open(img)
+
+            # resize
+            height,width = img.size
+            currentSize = len(img.fp.read())
+            # these numbers could probably be better
+            maxDim = math.floor(max([width,height]) * 4194304 / currentSize / 3)
+            img.thumbnail((maxDim, maxDim))
+
+
+            # convert back to byte object for classify_image
+            imgByteArr = BytesIO()
+            img.save(imgByteArr, format='PNG')
+            imgByteArr = imgByteArr.getvalue()
+
+            results = predictor.classify_image(project.id, publish_iteration_name, imgByteArr)
+
+        # if image actually doesn't exist
+        except OSError:
+            pass
+
+    if results is not None:
+        for tag in results.predictions:
+            if tag.tag_name == 'caneToad':
+                percentage = tag.probability
+    else:
+        percentage = 'NA'
+
+    print(percentage)
+    image_predictions.append([url, species, percentage])
+
+# get in ascending order of probability
+sorted_predictions = sorted(image_predictions, key=lambda tup: tup[2])
+
+
+# write new file with image urls and prediction percentages
+with open('predictions/multitagsAll_predicted.csv', 'w') as myfile:
+    wr = csv.writer(myfile, delimiter = ',')
+    wr.writerows(sorted_predictions)
--- trainFromImageUrl.py
+++ trainFromImageUrl.py
@@ -5,6 +5,7 @@ from azure.cognitiveservices.vision.customvision.prediction import CustomVisionP
 from msrest.authentication import ApiKeyCredentials
 import GetALAimages
 import math
+import csv
 
 
 # setting up project using keys
@@ -24,19 +25,23 @@ for project in trainer.get_projects():
     if project.name == 'Cane Toad Classifier Python':
         break
 # iteration name must be changed each iteration to publish
-publish_iteration_name = "classifyModel_moreSpecies"
+publish_iteration_name = "classify_model_witholding_some_images"
 
 
 
-# comment
+# clear existing training images
+#trainer.delete_images(project.id, all_images=True, all_iterations=False)
+
 # training with image urls
 print("Adding images...")
 
+# initialise list which stores a bunch of labelled images for testing later
+test_list = []
 
 # getting image urls using ALA file for each species (iterative as we can only upload 64 images at a time)
 for species in ['caneToad', 'stripedMarshFrog', 'ornateBurrowingFrog', 'australianGreenTreeFrog', 'bumpyRockFrog',
                 'crawlingToadlet', 'daintyGreenTreeFrog', 'desertFroglet', 'desertTreeFrog', 'giantFrog', 'hootingFrog',
-                 'marbledFrog', 'moaningFrog', 'motorbikeFrog', 'newHollandFrog', 'rockholeFrog',
+                'longFootedFrog', 'marbledFrog', 'moaningFrog', 'motorbikeFrog', 'newHollandFrog', 'rockholeFrog',
                 'rothsTreeFrog', 'westernBanjoFrog', 'whiteLippedTreeFrog']:
 
     # finding tag id
@@ -46,40 +51,53 @@ for species in ['caneToad', 'stripedMarshFrog', 'ornateBurrowingFrog', 'australi
     if tag.name!=species:
         tag = trainer.create_tag(project.id, species)
 
+
     file_dir = 'ala image urls/' + species + 'RawFile.csv'
     image_url_list = GetALAimages.listOfAlaImageUrls(file_dir)
 
     # going through a small portion of url list as can only upload 64 at a time
-    for batch_number in range(math.ceil(len(image_url_list)/64)):
+    for batch_number in range(math.ceil(len(image_url_list)/67)):
         image_list = []
-        endIndex = (batch_number+1)*64
+        endIndex = (batch_number+1)*67
         if endIndex > len(image_url_list):
             endIndex = len(image_url_list)
-        for url in image_url_list[batch_number*64: endIndex]:
+        for url in image_url_list[batch_number*67: endIndex-3]:
             image_list.append(ImageUrlCreateEntry(url=url, tag_ids=[tag.id]))
+        # every 67 items, add 3 to the testing images, along with their actual species
+        for i in range(endIndex-3,endIndex):
+            if i>=0 and i<len(image_url_list):
+                test_list.append([image_url_list[i], species])
+
+        # check that there are some images to train on, then upload these
+        if len(image_list) > 0:
+            upload_result = trainer.create_images_from_urls(project.id, ImageUrlCreateBatch(images=image_list))
+
+            # gives error when there is a duplicate (which is possible with the ALA data) so code below is to ignore
+            # duplicate error.
+            while not upload_result.is_batch_successful:
+                image_list = []
+                for image in upload_result.images:
+                    if image.status == 'OK':
+                        # add to new new image_list with corresponding url and tag
+                        image_list.append(ImageUrlCreateEntry(url=image.source_url, tag_ids=[tag.id]))
+                    elif image.status != 'OKDuplicate':
+                        print("Image batch upload failed.")
+                        print("Image status: ", image.status)
+                        print("Image url: ", image.source_url)
+                if len(image_list)>0:
+                    upload_result = trainer.create_images_from_urls(project.id, ImageUrlCreateBatch(images=image_list))
+                else:
+                    break
 
 
-        upload_result = trainer.create_images_from_urls(project.id, ImageUrlCreateBatch(images=image_list))
 
-        # gives error when there is a duplicate (which is possible with the ALA data) so code below is to ignore
-        # duplicate error.
-        while not upload_result.is_batch_successful:
-            image_list = []
-            for image in upload_result.images:
-                if image.status == 'OK':
-                    # add to new new image_list with corresponding url and tag
-                    image_list.append(ImageUrlCreateEntry(url=image.source_url, tag_ids=[tag.id]))
-                elif image.status != 'OKDuplicate':
-                    print("Image batch upload failed.")
-                    print("Image status: ", image.status)
-                    print("Image url: ", image.source_url)
-            if len(image_list)>0:
-                upload_result = trainer.create_images_from_urls(project.id, ImageUrlCreateBatch(images=image_list))
-            else:
-                break
+# save urls for testing to a csv file
+#test_list = [[el] for el in test_list]
+with open('predictions/multitagsAll.csv', 'w') as myfile:
+    wr = csv.writer(myfile, delimiter = ',')
+    wr.writerows(test_list)
 
-
-
+exit(-1)
 
 import time
 
@@ -94,4 +112,3 @@ while (iteration.status != "Completed"):
 
 trainer.publish_iteration(project.id, iteration.id, publish_iteration_name, prediction_resource_id)
 print ("Done!")
-
